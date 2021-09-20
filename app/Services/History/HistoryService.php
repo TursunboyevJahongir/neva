aa716e3db8a2388c68d799f852f13429bc65f4fa
Jaxongir <jtsccorp@mail.ru>
2021-09-20 17:59:29 +0500

    analytic

--- app/Services/History/HistoryService.php
+++ app/Services/History/HistoryService.php
@@ -11,39 +11,5 @@ class HistoryService
 {
 
 
-    public function record(Request $request)
-    {
-        $stories = HistoryView::where('ip_address', $request->ip())
-            ->where('user_agent', $request->userAgent())
-            ->orderByDesc('id')
-            ->get();
-
-        if($stories->isNotEmpty())
-        {
-            $history = HistoryView::where('ip_address', $request->ip())
-                ->where('user_agent', $request->userAgent())
-                ->orderByDesc('id')
-                ->first();
-                if ($history->url !== $request->path()) {
-                    HistoryView::create([
-                        'ip_address' => $request->ip(),
-                        'user_agent' => $request->userAgent(),
-                        'url' => $request->path(),
-                        'start_time' => \Carbon\Carbon::now(),//date('Y-m-d H:i:s'),
-                        'end_time' =>\Carbon\Carbon::now(),// date('Y-m-d H:i:s')
-                    ]);
-                }
-            $history->end_time=\Carbon\Carbon::now();
-            $history->save();
-        }
-        else
-            HistoryView::create([
-                'ip_address' => $request->ip(),
-                'user_agent' => $request->userAgent(),
-                'url' => $request->path(),
-                'start_time' =>\Carbon\Carbon::now(), //date('Y-m-d H:i:s'),
-                'end_time' =>\Carbon\Carbon::now()// date('Y-m-d H:i:s')
-            ]);
-
-    }
+   
 }
