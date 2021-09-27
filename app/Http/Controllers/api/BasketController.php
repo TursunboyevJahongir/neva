90146a0a5e96bcf9d3dc4af56119ce22c67b3022
Jaxongir <jtsccorp@mail.ru>
2021-09-27 17:27:05 +0500

    basket

--- app/Http/Controllers/api/BasketController.php
+++ app/Http/Controllers/api/BasketController.php
@@ -14,47 +14,49 @@ use Illuminate\Http\Request;
 
 class BasketController extends ApiController
 {
-    public function __construct(
-        private BasketService $service
-)
+
+    private $service;
+
+    public function __construct(BasketService $service)
     {
+        $this->service = $service;
     }
 
-/**
- * Display a listing of the resource.
- *
- * @return \Illuminate\Http\Response
- */
-public
-function index(Request $request)
-{
-    $size = $request->per_page ?? 10;
-    $orderBy = $request->orderby ?? "created_at";
-    $sort = $request->sort ?? "DESC";
-    $baskets = $this->service->all($orderBy, $sort, $size);
-    return $this->success(__('pages.documents'), new PaginationResourceCollection($baskets['basket'], BasketResource::class), $baskets['append']);
-}
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public
+    function index(Request $request)
+    {
+        $size = $request->per_page ?? 10;
+        $orderBy = $request->orderby ?? "created_at";
+        $sort = $request->sort ?? "DESC";
+        $baskets = $this->service->all($orderBy, $sort, $size);
+        return $this->success(__('pages.documents'), new PaginationResourceCollection($baskets['basket'], BasketResource::class), $baskets['append']);
+    }
 
 
-/**
- * Store a newly created resource in storage.
- *
- * @param  \Illuminate\Http\Request $request
- * @return \Illuminate\Http\Response
- */
-public
-function store(BasketRequest $request)
-{
-    $this->service->cart($request->validated());
-    return $this->success(__('messages.success'));
-}
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public
+    function store(BasketRequest $request)
+    {
+        $this->service->cart($request->validated());
+        return $this->success(__('messages.success'));
+    }
 
-public
-function delete(BasketProductDeleteRequest $request)
-{
-    Basket::whereIn('id',$request->basket_id)->delete();
-    return $this->success(__('messages.success'));
-}
+    public
+    function delete(BasketProductDeleteRequest $request)
+    {
+        Basket::whereIn('id', $request->basket_id)->delete();
+        return $this->success(__('messages.success'));
+    }
 
 
 }
--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('verify.device_headers')->prefix('v1')->group(static function () {
+Route::middleware('verify.device_headers')->prefix('v1')->group(/**
+ *
+ */
+    static function () {
     /**
      * Login / Register
      */
