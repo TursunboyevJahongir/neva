ea48cd4f0941750dd9e34718ae5e37a875a8a2d6
Jaxongir <jtsccorp@mail.ru>
2021-10-04 17:59:18 +0500

    order

--- app/Http/Controllers/api/OrderController.php
+++ app/Http/Controllers/api/OrderController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\ApiController;
+use App\Http\Requests\api\OrderRequest;
 use App\Models\Category;
 use App\Models\Order;
 use App\Models\ProductVariation;
@@ -10,6 +11,7 @@ use App\Services\Orders\OrderService;
 use App\Services\Page\PageService;
 use App\Services\Payments\InvoiceService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class OrderController extends ApiController
 {
@@ -40,14 +42,19 @@ class OrderController extends ApiController
      * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(OrderRequest $request)
     {
         abort_if(!auth()->check(), 403);
         $orders = $this->orders->setOrderBasket();
         $this->orders->save([
-            'city' => 'fsrf',
-            'region' => 'fsrf',
-            'street' => 'fsrf',
+            'name' => $request['name'] ?? Auth::user()->full_name,
+            'phone' => $request['phone']?? Auth::user()->phone,
+            'location' => $request['location'],
+            'city' => $request['city']?? Auth::user()->full_name,
+            'region' => $request['region'],
+            'address' => $request['street']?? Auth::user()->address,
+            'street' => $request['street']?? Auth::user()->full_name,
+            'comment' => $request['comment'],
             'delivery' => 'idcourier',
         ]+$orders, $orders['items']);
         return [];
