656dd37100e18a75092744ff8a7c73fde74159b4
Jaxongir <jtsccorp@mail.ru>
2021-10-08 17:33:20 +0500

    merged

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\Category;
+use Exception;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class CategoryFactory extends Factory
@@ -14,6 +15,22 @@ class CategoryFactory extends Factory
      */
     protected $model = Category::class;
 
+    /**
+     * @throws Exception
+     */
+    public function configure()
+    {
+        $fake = $this->faker;
+        return $this->afterCreating(static function (Category $intro) use ($fake) {
+            @mkdir(public_path('/uploads/category/'), 0777, true);
+                $time = time() . random_int(1000, 60000);
+                copy($fake->imageUrl(), public_path('/uploads/category/') . $time . '.jpg');
+                $path = '/uploads/category/' . $time . '.jpg';
+                $intro->ico()->create([
+                    'url' => $path,
+                ]);
+        });
+    }
     /**
      * Define the model's default state.
      *
--- database/factories/ProductVariationFactory.php
+++ database/factories/ProductVariationFactory.php
@@ -8,6 +8,7 @@ use App\Models\ProductAttribute;
 use App\Models\ProductAttributeValue;
 use App\Models\ProductVariation;
 use App\Models\Shop;
+use Exception;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class ProductVariationFactory extends Factory
@@ -20,46 +21,64 @@ class ProductVariationFactory extends Factory
     protected $model = ProductVariation::class;
 
     /**
+     * @throws Exception
+     */
+    public function configure()
+    {
+        $faker = $this->faker;
+        return $this->afterCreating(static function (ProductVariation $product) use ($faker) {
+            @mkdir(public_path('/uploads/product/'), 0777, true);
+            $time = time() . random_int(1000, 60000);
+            copy($faker->imageUrl(), public_path('/uploads/product/') . $time . '.jpg');
+            $path = '/uploads/product/' . $time . '.jpg';
+            $product->image()->create([
+                'url' => $path,
+            ]);
+        });
+    }
+
+    /**
      * Define the model's default state.
      *
      * @return array
      */
     public function definition()
     {
+        $attribute = ProductAttribute::all()->random()->id;
+        $attribute2 = ProductAttribute::all()->random()->id;
         return [
             'product_id' => Product::all()->random()->id,
 //            'product_attribute_value_ids' => $this->faker->boolean ? [ProductAttributeValue::all()->random()->id] : [ProductAttributeValue::all()->random()->id, ProductAttributeValue::all()->random()->id],
             'product_attributes' => $this->faker->boolean ?
                 [[
-                    'attribute' => ProductAttribute::all()->random()->id,
+
+                    'attribute' => $attribute,
                     "values" =>
                         [
-                            ProductAttributeValue::all()->random()->id,
-                            ProductAttributeValue::all()->random()->id,
+                            ProductAttributeValue::where('product_attribute_id', $attribute)->get()->random()->id,
+                            ProductAttributeValue::where('product_attribute_id', $attribute)->get()->random()->id,
                         ]
                 ]] :
                 [
                     [
-                        'attribute' => ProductAttribute::all()->random()->id,
+                        'attribute' => $attribute,
                         "values" =>
                             [
-                                ProductAttributeValue::all()->random()->id,
-                                ProductAttributeValue::all()->random()->id,
-                            ],
+                                ProductAttributeValue::where('product_attribute_id', $attribute)->get()->random()->id,
+                                ProductAttributeValue::where('product_attribute_id', $attribute)->get()->random()->id,
+                            ]
                     ],
                     [
-                        'attribute' => ProductAttribute::all()->random()->id,
+                        'attribute' => $attribute2,
                         "values" =>
                             [
-                                ProductAttributeValue::all()->random()->id,
-                                ProductAttributeValue::all()->random()->id,
+                                ProductAttributeValue::where('product_attribute_id', $attribute2)->get()->random()->id,
+                                ProductAttributeValue::where('product_attribute_id', $attribute2)->get()->random()->id,
                             ]
                     ]
                 ],
             'quantity' => $this->faker->numberBetween(100, 150),
             'old_price' => $this->faker->numberBetween(10000, 500000),
-            'price' => $this->faker->numberBetween(400000, 10000000),
-            'percent' => $this->faker->boolean ? $this->faker->numberBetween(5, 50) : null,
-        ];
+            'price' => $this->faker->numberBetween(400000, 10000000),];
     }
 }
