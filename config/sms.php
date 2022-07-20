a181a1429e7480468f439d229add0d85aedc6e9d
Jahongir Tursunboyev <47606395+TursunboyevJahongir@users.noreply.github.com>
2022-07-20 19:33:20 +0500

    Update sms.php

--- config/sms.php
+++ config/sms.php
@@ -7,9 +7,9 @@
 
 return [
 
-    'api_url' => env('ETC_SMS_URL', 'http://sms.etc.uz:8083/'),
-    'sender' => env('ETC_SMS_SENDER', 'KIDYA'),
-    'login' => env('ETC_SMS_LOGIN', 'ist0002ts'),
-    'password' => env('ETC_SMS_PASSWORD', '37nrr291'),
-    'token' => env('ETC_SMS_TOKEN', '859ea2450228a5d655e3a8c2f9a5aedc30b591df'),
+    'api_url' => env('ETC_SMS_URL'),
+    'sender' => env('ETC_SMS_SENDER'),
+    'login' => env('ETC_SMS_LOGIN'),
+    'password' => env('ETC_SMS_PASSWORD'),
+    'token' => env('ETC_SMS_TOKEN'),
 ];
